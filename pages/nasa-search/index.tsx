1032d5fcefe4a4cba528a3c7bad8894044f5e739
Kevin Telles <kvnmt0@gmail.com>
2021-07-04 20:10:57 -0300

    feat: add /nasa-search page

---
+++ pages/nasa-search/index.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const TitleTest = styled.h1`
+  color: red;
+`
+
+export default function Home() {
+  return (
+    <TitleTest>NASA Search</TitleTest>
+  )
+}
